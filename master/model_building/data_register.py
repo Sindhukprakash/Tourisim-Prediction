8232a51d59119228612c94be44a5351e0ff29d8d
root <Sindhu>
2025-08-30 12:20:03 +0000

    upadted token

--- master/model_building/data_register.py
+++ master/model_building/data_register.py
@@ -2,12 +2,21 @@ from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 from huggingface_hub import HfApi, create_repo
 import os
 
+from dotenv import load_dotenv
 
-repo_id = "Sindhuprakash/Tourism-Prediction-DataSet"
+# Load the .env file
+load_dotenv()
+
+# Access variables
+hf_token = os.getenv("HF_TOKEN")
+print(f"Hugging Face Token loaded: {hf_token is not None}") # Add this line to check if token is loaded
+
+
+repo_id = "sindhuprakash/Tourism-Prediction-DataSet"
 repo_type = "dataset"
 
 # Initialize API client
-api = HfApi(token=os.getenv("HF_TOKEN"))
+api = HfApi()
 
 # Step 1: Check if the space exists
 try:
