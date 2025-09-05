432a7296b31feccb6073887cc185053b4d8ba1fd
Sindhukprakash <sindhuprakash05@gmail.com>
2025-09-05 15:26:17 +0000

    Updating the data.py files

--- master/hosting/hosting.py
+++ master/hosting/hosting.py
@@ -1,10 +1,33 @@
 from huggingface_hub import HfApi
 import os
 
-api = HfApi(token=os.getenv("HF_TOKEN"))
-api.upload_folder(
-    folder_path="master/deployment",     # the local folder containing your files
-    repo_id="sindhuprakash/Tourism-Prediction",          # the target repo
-    repo_type="space",                      # dataset, model, or space
-    path_in_repo="",                          # optional: subfolder path inside the repo
+# Authenticate with token
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+# Access variables
+hf_token = os.getenv("HF_TOKEN")
+if hf_token is None:
+    raise ValueError("Hugging Face token not found in .env file")
+else:
+  print("HF_TOKEN loaded:", hf_token[:8], "...")
+from huggingface_hub import login
+
+#login(token=hf_token)
+
+api = HfApi(token = hf_token)
+repo_id = "Sindhuprakash/Tourism-Prediction-Space"
+
+try:
+  api.upload_folder(
+    folder_path="master/deployment/",  # the local folder containing your files
+    repo_id=repo_id,          # the target repo
+    repo_type="Space",             # dataset, model, or space
+    path_in_repo="deployment",                          # optional: subfolder path inside the repo
 )
+except Exception as e:
+  print(f"Error uploading file: {e}")
+
+
